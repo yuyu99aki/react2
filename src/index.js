585fdc5d9897056dbb4bd23fac0e3e6f0b909049
yuyu99aki <yu99-a@dream.ocn.ne.jp>
2023-02-05 15:27:21 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  alert(inputText);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
